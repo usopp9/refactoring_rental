5b4ed422271ee813004d8fc2322c45e0b02ccf73
usopp <usopp9@naver.com>
2018-04-09 11:23:23 +0900

    메서드 이동 적용 Customer->Rental

--- src/main/java/refactoring_rental/Customer.java
+++ src/main/java/refactoring_rental/Customer.java
@@ -29,7 +29,7 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName() + "고객님의 대여기록\n");
 		
 		for(Rental each:rentals) {
-			double thisAmount =amountFor(each);
+			double thisAmount =each.getCharge();
 			frequentRenterPoints++;
 			
 			if((each.getMovie().getPriceCode() ==Movie.NEW_RELEASE) && each.getDaysRented() >1) 
@@ -45,28 +45,4 @@ public class Customer {
 		
 		return result.toString();
 	}
-	private double amountFor(Rental aRental) {
-		double result = 0;
-		
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result +=2;
-			if(aRental.getDaysRented()>2) {
-				result += (aRental.getDaysRented() -2)*1.5;
-			}
-				break;
-		case Movie.NEW_RELEASE:
-			result += aRental.getDaysRented() *3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if(aRental.getDaysRented() >3) {
-				result +=(aRental.getDaysRented() -3) *1.5;
-			}
-			break;
-		default:
-			break;
-		}
-		return result;
-}
 }
