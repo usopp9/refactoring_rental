c8a743b37bd34afdba7fb208ba1d25473d04a729
usopp <usopp9@naver.com>
2018-04-10 09:45:23 +0900

    메서드 이동(Rental->Movie)

--- src/main/java/refactoring_rental/Movie.java
+++ src/main/java/refactoring_rental/Movie.java
@@ -24,6 +24,37 @@ public class Movie {
 	public void setPriceCode(int priceCode) {
 		this.priceCode = priceCode;
 	}
+	double getCharge(int aDaysRented) {
+		double result = 0;
+		
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			result +=2;
+			if(aDaysRented>2) {
+				result += (aDaysRented -2)*1.5;
+			}
+				break;
+		case Movie.NEW_RELEASE:
+			result += aDaysRented *3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if(aDaysRented >3) {
+				result +=(aDaysRented-3) *1.5;
+			}
+			break;
+		default:
+			break;
+		}
+		return result;
+	}
+	int getFrequentRentalPoints(int aDaysRented) {
+		if((getPriceCode() ==Movie.NEW_RELEASE) && aDaysRented >1) {
+			return 2;
+		}else {
+			return 1;
+		}
+	}
 	
 	
 }
